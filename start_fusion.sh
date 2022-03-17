dc8e8cf03106b4a060753a12be2f92b87e9c1e86
kissw <kissw@hanyang.ac.kr>
2022-03-18 00:55:54 +0900

    add track.world

--- start_fusion.sh
+++ start_fusion.sh
@@ -33,6 +33,12 @@ elif [ "$1" == "mcity_jaerock" ] ; then
 elif [ "$1" == "simple_city" ] ; then
     echo "Starting with $1..." #### experimental
     roslaunch fusion sitl.launch world:=$1 x:=-35 y:=-1.6 z:=0 R:=0 P:=0 Y:=0
+elif [ "$1" == "track_oa_train" ] ; then
+    echo "Starting with $1..." #### experimental
+    roslaunch fusion sitl.launch world:=$1 x:=0 y:=0 z:=0.3 R:=0 P:=0 Y:=0
+elif [ "$1" == "track_oa_test" ] ; then
+    echo "Starting with $1..." #### experimental
+    roslaunch fusion sitl.launch world:=$1 x:=0 y:=0 z:=0.3 R:=0 P:=0 Y:=0
 else 
     echo "Error: no $1.world file exist." 
 fi
